3737c0a85f69596b1420b5afa0ce08c2ca3738d0
kathleen.e.burns@gmail.com <kathleen.e.burns@gmail.com>
2019-06-10 13:52:02 -0500

    completed JS to add new row to each table

--- app.js
+++ app.js
@@ -13,3 +13,31 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// add new deal to table
+function newLocalDeal() {
+  var table = document.getElementById("local");
+  var row = table.insertRow(3);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  cell1.innerHTML = "New deal";
+  cell2.innerHTML = "$100";
+}
+
+function newGetawayDeal() {
+  var table = document.getElementById("getaway");
+  var row = table.insertRow(3);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  cell1.innerHTML = "New deal";
+  cell2.innerHTML = "$100";
+}
+
+function newGoodsDeal() {
+  var table = document.getElementById("goods");
+  var row = table.insertRow(3);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  cell1.innerHTML = "New deal";
+  cell2.innerHTML = "$100";
+}
